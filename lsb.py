15b5ae49dcb56fdcb643a949539c63ade86b662f
rthandi <rthandi1@sheffield.ac.uk>
2020-02-05 16:13:50 +0000

    Updated to write image to disk

--- lsb.py
+++ lsb.py
@@ -3,19 +3,13 @@ import cv2 as cv
 
 img = cv.imread('img/parrot.png', 0)
 
-print(img)
-
 # message = input("Type in the message that you would like to be hidden")
 message = "hello this is a test"
-
-
 
 binMessage = []
 
 for i in message:
     binMessage.append(format(ord(i), '08b'))
-
-print(binMessage)
 
 #need to change loop parameters - currently goes by length of message not the length of the image as it should be
 for i in range(0, len(binMessage) - 1):
@@ -27,6 +21,8 @@ for i in range(0, len(binMessage) - 1):
         img[i][j] = int(byte[0:-1] + str(updatedLastBit), 2)
         print(img[i][j])
 
-cv.imshow('stegoImage', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+if cv.imwrite('stegoImg/parrot.png', img):
+    cv.imshow('stegoImage', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
