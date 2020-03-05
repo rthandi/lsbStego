0862d2b86f422bd718c45691c963220031fa98f8
rthandi <rthandi1@sheffield.ac.uk>
2020-03-05 14:12:18 +0000

    Completed 1 embed rate lsb stegonography.

--- readLsb.py
+++ readLsb.py
@@ -1,9 +1,8 @@
-import numpy as np
 import cv2 as cv
 import queue
-import binascii
 
-def compareQueue(queue1, queue2):
+
+def compare_queue(queue1, queue2):
     return queue1.queue == queue2.queue
 
 img = cv.imread('stegoImg/parrot.png', 0)
@@ -24,7 +23,6 @@ lengthStack = queue.LifoQueue()
 
 flag = True
 
-print("here1")
 # The indicator is 16 length so we can fill the first 16 by default
 for i in range(16):
     nextVal = lsbQueue.get()
@@ -34,72 +32,49 @@ for i in range(16):
         comparisonQueue.put('1')
     past16.put(nextVal)
     lengthStack.put(nextVal)
-    print(nextVal)
     flag = not flag
-
-print("here1.5")
 
 # we do a check here for every byte only as this minimises errors in reading lengths when part of the indicator is in
 # the binary string for the length eg. length is 110110101 - the 0101 would trigger the logic of checking the length of
 # the indicator and cause incorrect lengths to be read. Now this only happens if the length is 01010101 which is
 # unlikely and can have its own specific logic if needed
-while not compareQueue(past16, comparisonQueue):
+while not compare_queue(past16, comparisonQueue):
     # read a byte
     for i in range(8):
         nextVal = lsbQueue.get()
         lengthStack.put(nextVal)
         past16.get()
         past16.put(nextVal)
-#
-# while not compareQueue(past16, comparisonQueue):
-#     # print(compareQueue(past16, comparisonQueue))#
-#     nextVal = lsbQueue.get()
-#     print(nextVal)
-#     lengthStack.put(nextVal)
-#     past16.get()
-#     past16.put(nextVal)
 
-print("here2")
 # removes the indicator from the length stack
 for i in range(16):
-    print(lengthStack.get())
-print("here3")
-
-print(lengthStack.qsize())
+    lengthStack.get()
 
 length = ''
 
 while not lengthStack.empty():
     length += lengthStack.get()
 
-print(length)
-
 #reverse string
 length = length[::-1]
 lengthInt = int(length, 2)
-print(lengthInt)
 
-length = int(length)
-stringBuilder = ''
+tempArray = []
 
-# while length > 0:
-#     byteBuffer = ''
-#     for i in range(8):
-#         length -= 1
-#         byteBuffer += lsbQueue.get()
-#     stringBuilder += binascii.b2a_qp(byteBuffer)
+# extract all of the bytes individually
+while lengthInt > 0:
+    stringBuilder = ''
+    for i in range(8):
+        stringBuilder += lsbQueue.get()
+    tempArray.append(stringBuilder)
+    lengthInt -= 1
 
-for i in range(length):
-    stringBuilder += lsbQueue.get()
+outputString = ''
 
-binary_int = int(stringBuilder, 2)
-byte_number = binary_int.bit_length() + 7 // 8
+# Convert each byte to its ascii value
+for i in tempArray:
+    n = int('0b' + i, 2)
+    n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+    outputString += str(chr(n))
 
-binary_array = binary_int.to_bytes(byte_number, "big")
-ascii_text = binary_array.decode()
-
-print(ascii_text)
-
-print(stringBuilder)
-
-# while compareQueue(pas)
+print(outputString)
